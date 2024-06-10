f92aadc9d82619e948a20e3552fdea37e8e4a97a
AmosPeterAlex <amospeteralex28@gmail.com>
2024-06-10 16:32:58 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/nexteons/nexteons_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nexteons.nexteons_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/amos/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Amos extends StatelessWidget {
+  const Amos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
