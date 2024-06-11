e17750ef8361021c668c004adff182066e5debae
anushtp04 <anushtp04@gmail.com>
2024-06-11 14:31:55 +0530

    commit3

--- lib/anush/view.dart
+++ lib/anush/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:nexteons_widgets/anush/data_grid/datagrid.dart';
 import 'package:nexteons_widgets/sajin/buttons/filled%20_button.dart';
+import 'package:nexteons_widgets/sajin/buttons/icon_button.dart';
 import 'package:nexteons_widgets/sajin/buttons/outline_button.dart';
 
 class MyDataGridPage extends StatefulWidget {
@@ -10,8 +11,6 @@ class MyDataGridPage extends StatefulWidget {
 }
 
 class _MyDataGridPageState extends State<MyDataGridPage> {
-
-
   late CustomerDataSource customerDataSource;
 
   @override
@@ -27,55 +26,141 @@ class _MyDataGridPageState extends State<MyDataGridPage> {
         backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          toolbarHeight:167 ,
+          toolbarHeight: 150,
           title: Text("APP BAR"),
           centerTitle: true,
         ),
         body: Container(
             height: size.height,
             width: size.width,
-            padding: EdgeInsets.only(left: 44,right: 44,top: 30,bottom: 30),
+            padding: EdgeInsets.only(left: 44, right: 44, top: 30, bottom: 30),
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("SALES RECEIPT",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700),),
+                  Text(
+                    "SALES RECEIPT",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+                  ),
                   Gap(25),
                   Container(
                     width: double.infinity,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Colors.grey.shade400),
-                        color: Color.fromARGB(255, 252, 252, 252)
-                    ),
-                    padding: EdgeInsets.only(left: 44,right: 44,top: 30,bottom: 30),
-                    child: const Column(
+                        color: Color.fromARGB(255, 252, 252, 252)),
+                    padding: EdgeInsets.only(left: 44, right: 44, top: 30, bottom: 30),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Row(
+                        const Row(
                           children: [
-                            CustomFilledButton(buttonText: "NEW SALES RECEIPT",icon: Icons.add,textColor: Colors.white,iconColor: Colors.white,),
+                            CustomFilledButton(
+                              buttonText: "NEW SALES RECEIPT",
+                              icon: Icons.add,
+                              textColor: Colors.white,
+                              iconColor: Colors.white,
+                            ),
                             Spacer(),
-                            CustomOutlineButton(icon: Icons.search,buttonText: "Search" ,),
+                            CustomOutlineButton(
+                              icon: Icons.search,
+                              buttonText: "Search",
+                            ),
                             Gap(7),
-                            CustomOutlineButton(icon: Icons.filter,buttonText: "Filer" ,),
+                            CustomOutlineButton(
+                              icon: Icons.filter,
+                              buttonText: "Filer",
+                            ),
                             Gap(7),
-                            CustomOutlineButton(icon: Icons.calendar_month,buttonText: "Filer" ,),
+                            CustomOutlineButton(
+                              icon: Icons.calendar_month,
+                              buttonText: "Filer",
+                            ),
                             Gap(7),
-                            CustomOutlineButton(icon: Icons.screen_share,buttonText: "Share" ,),
+                            CustomOutlineButton(
+                              icon: Icons.screen_share,
+                              buttonText: "Share",
+                            ),
                             Gap(7),
-                            CustomOutlineButton(icon: Icons.arrow_drop_down,buttonText: "Bulk Action" ,),
+                            CustomOutlineButton(
+                              icon: Icons.arrow_drop_down,
+                              buttonText: "Bulk Action",
+                            ),
                           ],
                         ),
                         Gap(27),
-                       CustomDataGrid()
+
+                        ///data grid item
+
+                        CustomDataGrid(),
+
+                        ///
+                        Gap(22),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                          Text("1 of 10 pages", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Spacer(),
+                          CustomIconButton(
+                            icon: Icons.arrow_back_ios,
+                          ),
+                          CustomIconButton(
+                            icon: Icons.arrow_back_ios,
+                          ),
+                          CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.blue.shade800,
+                            child: Text(
+                              "1",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Gap(10),
+                          CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.blue.shade800,
+                            child: Text(
+                              "2",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Gap(10),
+                          Text("....",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                          Gap(10),
+                          CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.blue.shade800,
+                            child: Text(
+                              "10",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          CustomIconButton(
+                            icon: Icons.arrow_forward_ios,
+                          ),
+                          CustomIconButton(
+                            icon: Icons.arrow_forward_ios,
+                          ),
+
+                        ]),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CustomFilledButton(icon: Icons.add,buttonText: "NEW ACCOUNT GROUP",),
+                            Gap(15),
+                            CustomFilledButton(icon: Icons.add,buttonText: "NEW ACCOUNT",textFontSize: 16,),
+
+                          ],
+                        )
                       ],
                     ),
                   ),
                 ],
               ),
-            )
-        ));
-
+            )));
   }
 }
